ec296f3b089dbe6eeea198a5c014f1b076d3cd78
Jon Morton <jon.morton@gmail.com>
2010-08-05 19:57:31 -0400

    Added excanvas dependency

--- circular-layout.js
+++ circular-layout.js
@@ -11,16 +11,9 @@ function CircularLayout(graph) {
 CircularLayout.prototype.layout = function() {
   var positions = [];
   
-  // If a node is selected, only enough room needs to be made
-  // for N-1 nodes.
-  if (this.graph.display.selection == null) {
-    var count = ( (this.graph.nodeCount() ) );
-  } else {
-    var count = ( (this.graph.nodeCount() - 1) );
-  }
-  
   // Decide the increment amount and a radius relative to the
   // number of nodes to display.
+  var count = ( (this.graph.nodeCount() ) );
   var increment = (Math.PI*2) / count;
   var radius = this.graph.display.nodeSize/2.5 * count;
   var step = 0;
@@ -32,14 +25,12 @@ CircularLayout.prototype.layout = function() {
   // Place each node on the edge of a circle, taking care to skip over
   // the selected node without leaving a gap in it's place.
   for (key in this.graph.reachable) {
-    if (this.graph.display.selection != this.graph.reachable[key]) {
-      theta = step * increment;
-      x = radius * Math.cos(theta);
-      y = radius * Math.sin(theta);
-      this.graph.nodes[key].x = x;
-      this.graph.nodes[key].y = y;
-      step++;
-    }
+    theta = step * increment;
+    x = radius * Math.cos(theta);
+    y = radius * Math.sin(theta);
+    this.graph.nodes[key].x = x;
+    this.graph.nodes[key].y = y;
+    step++;
   }
   
   // Place the selected node in the center of the circle
